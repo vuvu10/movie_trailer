dcfdcfff454eb2e7b1390820d960e012e55243a8
DC <dcsiona@gmail.com>
2019-01-06 22:26:39 -0500

    Update Media.py

--- Media.py
+++ Media.py
@@ -1,14 +1,11 @@
 import webbrowser
 
 
-
-
-
-
 class movies():
-    #Below is the class for the movies#
+	"""Below is the class for the movies"""
 
-    def __init__(self, movie_title, movie_description, movie_poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_description, 
+    			 movie_poster_image, trailer_youtube):
         self.title = movie_title
         self.description = movie_description
         self.poster_image_url = movie_poster_image
@@ -16,4 +13,3 @@ class movies():
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-
